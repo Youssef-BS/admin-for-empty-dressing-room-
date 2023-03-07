72825295ed684a394abea02121a8c2b42e6aa104
Djosef0 <youssefbensaid827@gmail.com>
2023-03-07 23:54:17 +0100

    add function handle login to file login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,23 @@
-import React,{useEffect , useState} from 'react'
+import React,{useEffect , useState , useContext} from 'react'
 import "./login.css"
+import { AuthContext } from "../../context/authContext";
 
 
 const Login = () => {
+  const { login } = useContext(AuthContext);
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+
+  const handleLogin = async (event)=>{
+    event.preventDefault();
+    // window.location.reload(false);
+    await login(email,password);
+   }
   return (
     <div className='login'>
     <h1>Login pour admin seulement</h1>
-    <input type="text" placeholder='email' />
-    <input type="password" placeholder='password' />
+    <input type="text" placeholder='email' onChange={(e)=> setEmail(e.target.value)} />
+    <input type="password" placeholder='password' onChange={(e)=> setPassword(e.target.value)} />
     <button>Login</button>
     </div>
   )
