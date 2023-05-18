6c89b1819bf96088576272c5de42ff0b95a18471
Djosef0 <youssefbensaid827@gmail.com>
2023-05-18 21:47:35 +0100

    a

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React,{useContext} from 'react';
 import {AuthContext} from "../../context/authContext";
-import {Link , useNavigate} from "react-router-dom"
+import {Link , useNavigate} from "react-router-dom";
 import {
   LineStyle,
   Timeline,
